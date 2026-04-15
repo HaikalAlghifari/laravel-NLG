7fee9b1e4baae73f41ecd0ca1b1461b0041fb6f4
Haikal <haikal@192.168.1.34>
2026-04-15 15:22:33 +0700

    fix update

--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -7,9 +7,13 @@
     <form action="{{ route('products.update', $product->id) }}" method="POST" class="bg-white p-4 rounded shadow">
         @csrf
         @method('PUT')
+        <label for="">Name</label>
         <input name="name" value="{{ $product->name }}" class="w-full mb-2 p-2 border">
+        <label for="">Price</label>
         <input name="price" value="{{ $product->price }}" class="w-full mb-2 p-2 border">
+        <label for="">Stock</label>
         <input name="stock" value="{{ $product->stock }}" class="w-full mb-2 p-2 border">
+        <label for="">Description</label>
         <textarea name="description" class="w-full mb-2 p-2 border">{{ $product->description }}</textarea>
         <button class="bg-yellow-500 text-white px-4 py-2 rounded">Update</button>
     </form>
